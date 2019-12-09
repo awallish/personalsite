78fefe77e65efda1d00c9245bc22dd7145c4bd17
alex <alex.wallish@gmail.com>
2019-12-09 18:48:56 -0500

    added /api/me

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const exphbs = require("express-handlebars");
 const handlebars = exphbs.handlebars;
 const app = express();
 const http = require("http").Server(app);
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 
 app.use(logger("dev"));
@@ -34,6 +34,47 @@ app.get("/projects", function(req, res) {
 
 app.get("/writing", function(req, res) {
     res.render("writing");
+})
+
+app.get("/api/me", function(req, res) {
+    res.send({
+        "name": "Alex Wallish",
+        "headline": "Software engineer",
+        "about": "I a software engineer with three years of experience.",
+        "pronouns": "He / Him / His",
+        "openToOpportunities": true,
+        "skills": [
+            "python",
+            "java",
+            "javascript",
+            "node",
+            "flask",
+            "spring",
+            "machine learning",
+        ],
+        "education" : [
+            {
+                "name": "University of Maryland",
+                "startMonth": 8,
+                "start-year": 2017,
+                "endMonth": 5,
+                "endYear": 2021,
+                "currentlyEnrolled": true,
+                "degreeType": "bachelor's",
+                "major": "Computer Science",
+                "GPA": 3.7
+            }
+        ],
+        "work": [
+            {
+                "company": "Barclays Investment Bank",
+                "role": "Software Engineering Intern",
+                "startMonth": 6,
+                "startYear": 2019,
+                "description": "Worked on the research team to develop tools for researchers to use to better understand the markets.",
+            }
+        ]
+    })
 })
 
 

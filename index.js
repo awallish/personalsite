3fc1fb98c4d988a34c26ecb3c68c9cbfcd7aa3a8
awallish <alex.wallish@gmail.com>
2020-06-07 21:10:54 -0400

    Version 2.0.0 ready for deploy

--- index.js
+++ index.js
@@ -2,10 +2,13 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const logger = require("morgan");
 const exphbs = require("express-handlebars");
+const request=require('request');
+const csv=require('csvtojson');
 const handlebars = exphbs.handlebars;
 const app = express();
 const http = require("http").Server(app);
 const PORT = process.env.PORT || 5000;
+
 
 
 app.use(logger("dev"));
@@ -20,8 +23,14 @@ app.get("/", function(req, res) {
     res.render("home");
 })
 
-app.get("/poker", function(req, res) {
-    res.render("poker");
+app.get("/poker", async function(req, res) {
+
+
+    const jsonArray=await csv().fromStream(request.get("https://raw.githubusercontent.com/awallish/personalsite/master/poker.csv"));
+    console.log(JSON.stringify(jsonArray));
+    res.render("poker", {data: JSON.stringify(jsonArray)});
+
+
 })
 
 http.listen(PORT, function() {
